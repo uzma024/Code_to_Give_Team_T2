24aa70239e1bab887186571e73694c81c0a8eb20
Uzma FKhan <uzmafkhan24@gmail.com>
2022-06-14 15:51:43 +0530

    bug fixes

--- src/services/volunteerServices.js
+++ src/services/volunteerServices.js
@@ -7,17 +7,39 @@ let addmapping = (data) => {
                 Vid: data.Vid,
                 status: data.status
             };
-            
             DBConnection.query(
-                "INSERT INTO mapping set ?", map,
+                "select * from mapping where Aid=? and Vid=?",data.Aid,data.Vid,
                 function (err, rows) {
                     if (err) {
                       console.log(err);
                       reject(false);
                     }
+                    if(rows.length==0){
+                        DBConnection.query(
+                            "INSERT INTO mapping set ?", map,
+                            function (err, rows) {
+                                if (err) {
+                                  console.log(err);
+                                  reject(false);
+                                }
+                                resolve("Mapping added successfully");
+                              }
+                        );
+                    }else{
+                        DBConnection.query(
+                            "update mapping set status = ? where Vid = ? AND Aid= ?",
+                            ["accepted", volunteerDetailslist[i], req.params.id],
+                            (err) => {
+                                if (err) {
+                                console.log(err);
+                                }
+                            }
+                        );
+                    }
                     resolve("Mapping added successfully");
                   }
             );
+            
         
     });
 };
